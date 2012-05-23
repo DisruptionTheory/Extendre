0a965e7c1c8766a56f1a94de69ad0c2b796aa255
DarrellBailey <darrell@disruptiontheory.com>
2012-05-23 13:06:42 -0400

    Performance Improvements to some array extensions.

--- Extendre/Collections/ArrayExtensions.cs
+++ Extendre/Collections/ArrayExtensions.cs
@@ -51,18 +51,14 @@ public static class ArrayExtensions
     }
 
     /// <summary>
-    /// Sets each element in the array to the default constructed value for the type.
+    /// Sets each element in the array to the default value for the type.
     /// </summary>
     /// <typeparam name="T">The type of the array.</typeparam>
     /// <param name="array">The array to clear.</param>
     /// <returns>The cleared array.</returns>
     public static T[] Clear<T>(this T[] array)
     {
-        for (int i = 0; i <= array.Length; i++)
-        {
-            array[i] = default(T);
-        }
-        return array;
+        return new T[array.Length];
     }
 
     /// <summary>
@@ -75,11 +71,7 @@ public static class ArrayExtensions
     public static T[] Remove<T>(this T[] array, T item)
     {
         List<T> newCollection = new List<T>();
-        for (int i = 0; i <= array.Length; i++)
-        {
-            if(!array[i].Equals(item))
-                newCollection.Add(array[i]);
-        }
+        newCollection.RemoveAll(new Predicate<T>((comp) => comp.Equals(item)));
         return newCollection.ToArray();
     }
 
@@ -93,35 +85,21 @@ public static class ArrayExtensions
     public static T[] RemoveOnce<T>(this T[] array, T item)
     {
         List<T> newCollection = new List<T>();
-        for (int i = 0; i <= array.Length; i++)
-        {
-            if (!array[i].Equals(item))
-            {
-                newCollection.Add(array[i]);
-            }
-            else
-            {
-                newCollection.AddRange(array.GetAfter(i + 1));
-                return newCollection.ToArray();
-            }
-        }
+        newCollection.Remove(item);
         return newCollection.ToArray();
     }
 
     /// <summary>
-    /// Get the array of all elements after and including the specified index.
+    /// Get the array of all elements after the specified index.
     /// </summary>
     /// <typeparam name="T">The type of the array.</typeparam>
     /// <param name="array">The array to retrieve the values from.</param>
-    /// <param name="index">The index to start from.</param>
-    /// <returns>An array of all elements including and after the specified index.</returns>
+    /// <param name="index">The index for which all indicies after are returned.</param>
+    /// <returns>An array of all elements after the specified index.</returns>
     public static T[] GetAfter<T>(this T[] array, int index)
     {
-        T[] newArray = new T[array.Length - index];
-        for (int i = index, j = 0; i < array.Length; i++, j++)
-        {
-            newArray[j] = array[i];
-        }
+        T[] newArray = new T[array.Length - index -1];
+        Array.Copy(array, index + 1, newArray, 0, array.Length - index - 1);
         return newArray;            
     }
 
@@ -135,29 +113,37 @@ public static class ArrayExtensions
     public static T[] GetBefore<T>(this T[] array, int index)
     {
         T[] newArray = new T[index];
-        for (int i = 0; i < index; i++)
-        {
-            newArray[i] = array[i];
-        }
+        Array.Copy(array, 0, newArray, 0, index);
         return newArray;
     }
 
     /// <summary>
-    /// Get a section of the given array specefied by the lower and upper bounds.
+    /// Get a section of the given array specified by the lower and upper bounds.
     /// Both the lower and upper bounds will be included in the cut section.
     /// </summary>
     /// <typeparam name="T">The type of the array.</typeparam>
     /// <param name="array">The array to cut.</param>
     /// <param name="lowerBound">The lower bound of the section.</param>
     /// <param name="upperBound">The upper bound of the section.</param>
-    /// <returns>The specefied section of the array.</returns>
-    public static T[] GetSection<T>(this T[] array, int lowerBound, int upperBound)
+    /// <returns>The specified section of the array.</returns>
+    public static T[] GetBoundedSection<T>(this T[] array, int lowerBound, int upperBound)
     {
-        T[] newArray = new T[array.Length - (upperBound - lowerBound)];
-        for (int i = lowerBound, j = 0; i <= upperBound; i++, j++)
-        {
-            newArray[j] = array[i];
-        }
+        T[] newArray = new T[upperBound - lowerBound + 1];
+        Array.Copy(array, lowerBound, newArray, 0, upperBound - lowerBound + 1);
+        return newArray;
+    }
+
+    /// <summary>
+    /// Get a section of the array from a starting index through a count.
+    /// </summary>
+    /// <typeparam name="T">The type of the array.</typeparam>
+    /// <param name="array">The array to cut.</param>
+    /// <param name="lowerBound">The lower bound of the section.</param>
+    /// <param name="count">The number of elements to copy.</param>
+    /// <returns>The specified section of the array.</returns>
+    public static T[] GetCountedSection<T>(this T[] array, int lowerBound, int count) {
+        T[] newArray = new T[count];
+        Array.Copy(array, lowerBound, newArray, 0, count);
         return newArray;
     }
 
